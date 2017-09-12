143703b752d7e504fa1c6820fa5dae89f1a6832c
Hack Reactor Student <student@hackreactor.com>
2017-09-11 19:56:12 -0700

    Get data from Youtube API

--- src/components/search.js
+++ src/components/search.js
@@ -2,12 +2,26 @@ angular.module('video-player')
 
 .component('search', {
   templateUrl:`src/templates/search.html`,
-  controller: function() {
+  controller: function($http) {
     this.result = function(){
 
     };
     this.search = function(){
-      console.log('clicked');
+      var self = this;
+      $http({
+        method: "GET",
+        url: "https://www.googleapis.com/youtube/v3/search",
+        params: {
+          maxResults: '5',
+          key: YOUTUBE_API_KEY,
+          part: 'snippet',
+          q: self.query,
+          type: 'video'
+        }
+      }).then(function(response) {
+        console.log(response);
+      });
+      console.log(this.query);
     };
   }
 
--- src/components/videoList.js
+++ src/components/videoList.js
@@ -2,7 +2,7 @@ angular.module('video-player')
 .component('videoList', {
   bindings: {
     videos: '<',
-    hanldeVideoListEntryClick: '<'
+    onClick: '<'
   },
   templateUrl:`src/templates/videoList.html`,
   controller: function() {
